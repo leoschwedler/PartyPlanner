039723bb4d26de8c54019c35583b88f2f7a34647
leonardo schwuedller <77402907+leoschwedler@users.noreply.github.com>
2025-09-28 12:20:38 -0300

    first commit

---
+++ src/main/java/com/schwedlermobile/PartyPlanner/PartyPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.schwedlermobile.PartyPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schwedlermobile/PartyPlanner/PartyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schwedlermobile.PartyPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
